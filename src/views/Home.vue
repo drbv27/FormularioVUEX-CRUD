378d9159ea04abe5343123c3d227236ed6ed12b3
Diego <drbv27@gmail.com>
2021-12-27 19:35:25 -0500

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <form @submit.prevent="procesarFormulario">
+    <input
+      type="text"
+      class="form-control my-2"
+      placeholder="Ingrese nombre"
+      v-model.trim="tarea.nombre"
+    />
+    <div class="form-check form-check-inline">
+      <input
+        type="checkbox"
+        id="check-1"
+        class="form-check-input"
+        v-model="tarea.categorias"
+        value="javascript"
+      />
+      <label for="check-1" class="form-check-label">Javascript</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input
+        type="checkbox"
+        id="check-2"
+        class="form-check-input"
+        v-model="tarea.categorias"
+        value="node js"
+      />
+      <label for="check-2" class="form-check-label">Node.js</label>
+    </div>
+    <div class="mt-2">
+      <div class="form-check form-check-inline">
+        <input
+          type="radio"
+          id="radio-1"
+          class="form-check-input"
+          value="urgente"
+          v-model="tarea.estado"
+        />
+        <label for="radio-1" class="form-check-label">Urgente</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input
+          type="radio"
+          id="radio-2"
+          class="form-check-input"
+          value="relax"
+          v-model="tarea.estado"
+        />
+        <label for="radio-2" class="form-check-label">Relax</label>
+      </div>
+    </div>
+    <div class="mt-2">
+      <input
+        class="form-control"
+        type="number"
+        name=""
+        id=""
+        v-model="tarea.numero"
+      />
+    </div>
+    <button
+      class="btn btn-dark mt-2 btn-block"
+      type="submit"
+      :disabled="bloquear"
+    >
+      Procesar
+    </button>
+  </form>
+  <hr />
+  <p>{{ tarea }}</p>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      tarea: {
+        nombre: "",
+        categorias: [],
+        estado: "",
+        numero: 0,
+      },
+    };
+  },
+  methods: {
+    procesarFormulario() {
+      console.log(this.tarea);
+      if (this.tarea.nombre.trim() === "") {
+        console.log("vacio");
+        return;
+      }
+      console.log("No está vacio");
+      //enviar los datos
+
+      this.tarea = {
+        nombre: "",
+        categorias: [],
+        estado: "",
+        numero: 0,
+      };
+    },
+  },
+  computed: {
+    bloquear() {
+      return this.tarea.nombre.trim() === "" ? true : false;
+    },
+  },
+};
+</script>
